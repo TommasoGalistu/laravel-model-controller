164b5e613122107f7890f5cea98a37b74ea9eacf
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-09-11 16:11:32 +0200

    feat: insert card with data server

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
-    //
+   public function index()
+   {
+       // Recupera tutti i film dalla tabella 'movies'
+       $movies = Movie::all();
+
+       // Mostra i dati per il debug
+
+
+       // Passa i dati alla vista
+       return view('home', compact('movies'));
+   }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,26 @@
+
 @extends('layout.main')
 
 @section('content')
+<div class="container d-flex flex-wrap gap-4">
 
-<h1>ciao sono la home</h1>
+    @foreach ($movies as $movie)
+
+    <div class="card" style="width: 18rem;">
+
+        <div class="card-body">
+          <h3 class="card-title">{{ $movie->title }}</h3>
+          <h6 class="card-title">{{ $movie->original_title }}</h6>
+          <p>{{ $movie->nationality }}</p>
+          <p>{{ $movie->vote }}</p>
+          <p>{{ $movie->date }}</p>
+
+          <a href="#" class="btn btn-primary">Descrizione film</a>
+        </div>
+      </div>
+
+    @endforeach
+</div>
+
+
 @endsection
