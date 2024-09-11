3d2dc8dc9a936040e98e1a77e662379bf2d4cd09
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-09-11 14:51:48 +0200

    feat: create model and controller

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/js/app.js')
-    <title>Document</title>
+    <title>Progetto</title>
 </head>
 <body>
     <h1>ciao</h1>
